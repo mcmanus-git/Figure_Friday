3d026a55db37037caa394ec3c8cf29782dfd75a4
Michael McManus <melancholic_maclean@yahoo.com>
2024-07-20 23:05:59 -0400

    change theme

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = dash.Dash(
     __name__,
     suppress_callback_exceptions=True,
     external_stylesheets=[
-        dbc.themes.LUX,  # Dash Themes CSS
+        dbc.themes.FLATLY, # LUX,  # Dash Themes CSS
         FA621,  # Font Awesome Icons CSS
     ],
     title=APP_TITLE,
@@ -66,5 +66,5 @@ app.layout = dcc.Loading(  # <- Wrap App with Loading Component
 server = app.server
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
     
